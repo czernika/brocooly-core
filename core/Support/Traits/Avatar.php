1efed5bad0533461f2bdf91fdfba77536333075c
Ihar Aliakseyenka <aliha.devs@gmail.com>
2021-08-21 23:05:56 +0300

    feat: user avatar metabox as trait

---
+++ core/Support/Traits/Avatar.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brocooly\Support\Traits;
+
+use Brocooly\Support\Facades\Meta;
+
+trait Avatar
+{
+    public function avatar() {
+		$this->createFields(
+			'avatar',
+			esc_html__( 'User avatar', 'brocooly' ),
+			[
+				Meta::image( 'avatar_url', esc_html__( 'Image', 'brocooly' ) )
+					->set_value_type( 'url' ),
+			],
+			'side',
+		);
+	}
+}
