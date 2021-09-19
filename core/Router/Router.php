c1d7620abc4f3cce431d4ef49e9049cff3d5db1a
Aliakseyenka <aliha.devs@gmail.com>
2021-09-19 15:31:23 +0300

    fix: allow only some conditionals for routing

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -18,11 +18,55 @@ class Router
 
 	private bool $route_was_hit = false;
 
+	private array $allowedConditionals = [
+		'is_404',
+		'is_admin',
+		'is_archive',
+		'is_attachment',
+		'is_author',
+		'is_blog_admin',
+		'is_category',
+		'is_comment_feed',
+		'is_customize_preview',
+		'is_date',
+		'is_day',
+		'is_embed',
+		'is_favicon',
+		'is_feed',
+		'is_front_page',
+		'is_home',
+		'is_month',
+		'is_network_admin',
+		'is_page',
+		'is_page_template',
+		'is_paged',
+		'is_post_type_archive',
+		'is_preview',
+		'is_privacy_policy',
+		'is_robots',
+		'is_rtl',
+		'is_search',
+		'is_single',
+		'is_singular',
+		'is_ssl',
+		'is_sticky',
+		'is_tag',
+		'is_tax',
+		'is_time',
+		'is_trackback',
+		'is_user_admin',
+		'is_year'
+	];
+
 	public function __construct( Routes $routes ) {
 		$this->routes = $routes;
 	}
 
 	public function __call( $condition, $args ) {
+		if ( ! in_array( $condition, $this->allowedConditionals, true ) ) {
+			return;
+		}
+
 		[ $callback ] = $args;
 		$this->routes->addRoute( 'get', Str::snake( $condition ), $callback );
 	}
